69b90c76de57bad91df83d72fc2e24b5bce256c6
Matthias Klauer <matthias.klauer@sap.com>
2014-10-17 11:07:32 +0200

    Remove packages from git

--- init.el
+++ init.el
@@ -15,10 +15,14 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(custom-enabled-themes (quote (adwaita)))
+ '(delete-selection-mode nil)
+ '(dired-dwim-target t)
  '(global-auto-complete-mode t)
  '(grep-find-ignored-directories (quote ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" ".DS_Store")))
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
+ '(magit-repo-dirs (quote ("/data/sfsf/workspace/wsapi_v1/idl-wsapi_v1-service" "/root/soapUI")))
+ '(mark-even-if-inactive t)
  '(menu-bar-mode t)
  '(org-agenda-files (quote ("~/org/gtd.org")))
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("melpa" . "http://melpa.milkbox.net/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
@@ -26,6 +30,7 @@
  '(projectile-mode-line-lighter " Projectile")
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil)
+ '(transient-mark-mode 1)
  '(uniquify-buffer-name-style (quote post-forward) nil (uniquify)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
