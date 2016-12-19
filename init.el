ed87c29e5534e807153482addfb5e7e8d2877865
Matthias Klauer <matthias.klauer@sap.com>
2016-12-19 08:51:34 +0100

    Theme changed to misterioso; deactivated dsv config

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
 (load "01org.el")
 ;;(load "02ruby.el")
 ;;(load "03auto-complete.el")
-(load "04dsvn.el")
+;;(load "04dsvn.el")
 
 ;; set up plantuml for use in org files
 (setq org-plantuml-jar-path
@@ -18,9 +18,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (adwaita)))
+ '(custom-enabled-themes (quote (misterioso)))
+ '(custom-safe-themes
+   (quote
+    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(delete-selection-mode nil)
  '(dired-dwim-target t)
  '(global-auto-complete-mode t)
@@ -39,6 +44,7 @@
  '(menu-bar-mode t)
  '(org-agenda-files (quote ("~/org/")))
  '(org-babel-load-languages (quote ((sh . t) (plantuml . t) (emacs-lisp . t))))
+ '(org-export-backends (quote (html md)))
  '(org-mobile-files (quote ("~/Dropbox/org")))
  '(package-archives
    (quote
