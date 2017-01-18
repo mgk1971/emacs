91566ad3fe87160584b911308eb0354c228c95cb
Matthias Klauer <matthias.klauer@sap.com>
2017-01-18 13:39:48 +0100

    Turn off helm mode

--- init.el
+++ init.el
@@ -47,8 +47,8 @@
 (add-to-list 'load-path "~/.emacs.d/custom")
 (load "my_proxy")
 (load "my_org")
-(load "my_helm")
-;;(load "my_ido")
+;;(load "my_helm")
+(load "my_ido")
 (load "my_projectile")
 ;;(load "my_ruby")
 
@@ -62,7 +62,6 @@
   :ensure t
   :config
   (global-company-mode 1))
-
 
 (use-package magit
   :ensure t
