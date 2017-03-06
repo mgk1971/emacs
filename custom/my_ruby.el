8bb48a5608e09bcf4da0f6339d03c7beb6822413
Matthias Klauer <matthias.klauer@sap.com>
2017-03-06 12:11:52 +0100

    Enable enhanced ruby mode

--- custom/my_ruby.el
+++ custom/my_ruby.el
@@ -2,5 +2,13 @@
 (use-package enh-ruby-mode
   :ensure t
   :config
+  (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
   (add-to-list 'auto-mode-alist
 	       '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode)) )
+  )
+
+(use-package robe
+  :ensure t
+  :config
+  (add-hook 'enh-ruby-mode-hook 'robe-mode)
+  )
