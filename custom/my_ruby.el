4e3565f6c570d470290906036eca7017c595a76a
Matthias Klauer <matthias.klauer@sap.com>
2017-03-06 14:29:16 +0000

    Fix parenthesis error in my_ruby.el

--- custom/my_ruby.el
+++ custom/my_ruby.el
@@ -4,7 +4,8 @@
   :config
   (add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
   (add-to-list 'auto-mode-alist
-	       '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode)) )
+	       '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode)
+	       )
   )
 
 (use-package robe
