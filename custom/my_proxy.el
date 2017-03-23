254813f977d395164dad468c973ad88161627d9b
Matthias Klauer <matthias.klauer@sap.com>
2017-03-23 10:38:57 +0100

    Removed use-package from proxy settings

--- custom/my_proxy.el
+++ custom/my_proxy.el
@@ -1,10 +1,9 @@
 ;; In SAP intranet we need a proxy for reaching the package servers
-(use-package url
-  :config
-  (setq url-proxy-services
-	(quote
-	 (("http" . "proxy.wdf.sap.corp:8080")
-	  ("https" . "proxy.wdf.sap.corp:8080"))
-	 )
-	)
-  )
+(require 'url-vars)
+(setq url-proxy-services
+      (quote
+       (("http" . "proxy.wdf.sap.corp:8080")
+	("https" . "proxy.wdf.sap.corp:8080"))
+       )
+      )
+
